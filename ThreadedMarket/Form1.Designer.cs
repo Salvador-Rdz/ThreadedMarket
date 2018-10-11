9eadf957b9e3ee4089d668578ace3a60efdee29e
Salvador-Rdz <rodriguez.salazar.salvador@gmail.com>
2018-10-10 20:03:23 -0500

    Final

--- ThreadedMarket/Form1.Designer.cs
+++ ThreadedMarket/Form1.Designer.cs
@@ -29,11 +29,333 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startBt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.endBT = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBt
+            // 
+            this.startBt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.startBt.Location = new System.Drawing.Point(331, 599);
+            this.startBt.Name = "startBt";
+            this.startBt.Size = new System.Drawing.Size(89, 32);
+            this.startBt.TabIndex = 0;
+            this.startBt.Text = "Start";
+            this.startBt.UseVisualStyleBackColor = true;
+            this.startBt.Click += new System.EventHandler(this.startBt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Apples";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Oranges";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Grapes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 452);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Watermelons";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(60, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(60, 164);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(60, 325);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(60, 468);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(101, 104);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(4, 87);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(55, 75);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // endBT
+            // 
+            this.endBT.Enabled = false;
+            this.endBT.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.endBT.Location = new System.Drawing.Point(466, 599);
+            this.endBT.Name = "endBT";
+            this.endBT.Size = new System.Drawing.Size(89, 32);
+            this.endBT.TabIndex = 13;
+            this.endBT.Text = "End";
+            this.endBT.UseVisualStyleBackColor = true;
+            this.endBT.Click += new System.EventHandler(this.endBT_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(4, 244);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(55, 75);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(646, 110);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 158);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(646, 110);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(182, 462);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(646, 110);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(182, 319);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(646, 110);
+            this.textBox4.TabIndex = 17;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(4, 533);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(55, 75);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 20;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(4, 376);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(55, 75);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 19;
+            this.pictureBox8.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(96, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "-/-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(96, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "-/-";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(96, 396);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "-/-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(96, 539);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "-/-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(864, 643);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.endBT);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startBt);
+            this.Name = "Form1";
+            this.Text = "Market Stalls";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startBt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button endBT;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- ThreadedMarket/Form1.cs
+++ ThreadedMarket/Form1.cs
@@ -7,14 +7,205 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using System.Collections;
 
 namespace ThreadedMarket
 {
+
     public partial class Form1 : Form
     {
+        public bool isRunning = false;
+        public MarketStall apples;
+        public MarketStall oranges;
+        public MarketStall grapes;
+        public MarketStall watermelons;
+        Thread appleStallP;
+        Thread appleStallC;
+        Thread orangeStallP;
+        Thread orangeStallC;
+        Thread grapeStallP;
+        Thread grapeStallC;
+        Thread watermelonStallP;
+        Thread watermelonStallC;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false; //This handy line helps us access resources inbetween threads.
+            timer1.Interval = 500;
+        }
+
+        private void startBt_Click(object sender, EventArgs e)
+        {
+            startBt.Enabled = false;
+            endBT.Enabled = true;
+            apples = new MarketStall(0, "apple", textBox1, 6000, 4000,10);
+            oranges = new MarketStall(2, "orange", textBox2, 4000, 1000,16);
+            grapes = new MarketStall(3, "grape", textBox4, 2000, 5000,20);
+            watermelons = new MarketStall(5, "watermelon", textBox3, 8000, 10000,8);
+            Thread.Sleep(3000);
+            appleStallP = new Thread(new ThreadStart(apples.produce));
+            appleStallC = new Thread(new ThreadStart(apples.consume));
+            orangeStallP = new Thread(new ThreadStart(oranges.produce));
+            orangeStallC = new Thread(new ThreadStart(oranges.consume));
+            grapeStallP = new Thread(new ThreadStart(grapes.produce));
+            grapeStallC = new Thread(new ThreadStart(grapes.consume));
+            watermelonStallP = new Thread(new ThreadStart(watermelons.produce));
+            watermelonStallC = new Thread(new ThreadStart(watermelons.consume));
+            appleStallP.Start();
+            appleStallC.Start();
+            orangeStallP.Start();
+            orangeStallC.Start();
+            grapeStallP.Start();
+            grapeStallC.Start();
+            watermelonStallP.Start();
+            watermelonStallC.Start();
+            isRunning = true;
+            timer1.Start();
+        }
+
+        private void endBT_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+            startBt.Enabled = true;
+            endBT.Enabled = false;
+            appleStallP.Abort();
+            appleStallC.Abort();
+            orangeStallP.Abort();
+            orangeStallC.Abort();
+            grapeStallP.Abort();
+            grapeStallC.Abort();
+            watermelonStallP.Abort();
+            watermelonStallC.Abort();
+            textBox1.AppendText("\r\nThe Apple Stand has closed");
+            textBox2.AppendText("\r\nThe Orange Stand has closed");
+            textBox4.AppendText("\r\nThe Grape Stand has closed");
+            textBox3.AppendText("\r\nThe Watermelon Stand has closed");
+        }
+        public class MarketStall
+        {
+            private  Semaphore fillCount;
+            private  Semaphore emptyCount;
+            private  Mutex mut = new Mutex();
+            public ArrayList fruits;
+            int nFruits=0;
+            int maxFruits = 0;
+            int count = 0;
+            int prWait = 0;
+            int coWait = 0;
+            bool isRunning=false;
+            string tFruit="undefined";
+            TextBox tBox;
+            public MarketStall(int nFruits, string tFruit, TextBox tBox, int prWait, int coWait, int maxFruits)
+            {
+                this.nFruits = nFruits;
+                this.tFruit = tFruit;
+                this.tBox = tBox;
+                this.prWait = prWait;
+                this.coWait = coWait;
+                this.maxFruits = maxFruits;
+                fruits = new ArrayList(maxFruits);
+                for (int i =1;i<=nFruits;i++)
+                {
+                    fruits.Add("" + tFruit + "" + i);
+                    count++;
+                }
+                fillCount = new Semaphore(maxFruits-nFruits, maxFruits);
+                emptyCount = new Semaphore(nFruits, maxFruits);
+                tBox.Text = "Fruit market stall has started with " + nFruits + " " + tFruit + "s";
+            }
+            [MethodImpl(MethodImplOptions.Synchronized)]
+            private bool isFull()
+            {
+                return fruits.Capacity== this.nFruits;
+            }
+
+            private bool isEmpty()
+            {
+                return fruits.Count == 0;
+            }
+
+            public void produce()
+            {
+                while (true)
+                {
+                    if (isFull())
+                    {
+                        tBox.AppendText("\r\n" + tFruit + " stall is full, Farmer is waiting");
+                        Thread.Sleep(coWait);
+                        Thread.Yield();
+                    }
+                        mut.WaitOne();
+                        fillCount.WaitOne();
+                        count++;
+                        string fruit = "" + tFruit + "" + count;
+                        fruits.Add(fruit); //Critical area
+                        nFruits++;
+                        tBox.AppendText("\r\n" + "Farmer has produced " + fruit);
+                        emptyCount.Release(1);
+                        mut.ReleaseMutex();
+                        int rWait = new Random().Next(1000, prWait);
+                        Thread.Sleep(rWait);
+                }
+            }
+
+            public void consume()
+            {
+                while (true)
+                {
+                    if (isEmpty())
+                    {
+                        tBox.AppendText( "\r\n" + tFruit + " stall is empty, customer is waiting");
+                        Thread.Sleep(prWait);
+                        Thread.Yield();
+                    }
+                    emptyCount.WaitOne();
+                    mut.WaitOne();
+                    string fruit = fruits[0].ToString();
+                    fruits.RemoveAt(0);
+                    nFruits--;
+                    mut.ReleaseMutex();
+                    fillCount.Release(1);
+                    tBox.AppendText("\r\nClient has purchased " + fruit);
+                    int rWait = new Random().Next(1000, coWait);
+                    Thread.Sleep(rWait);
+                }
+            }
+            public int getFruitCount()
+            {
+                return nFruits;
+            }
+            public int getMaxFruits()
+            {
+                return maxFruits;
+            }
+            public void setRunning(bool isRunning)
+            {
+                this.isRunning = isRunning;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(isRunning)
+            {
+                
+                label5.Text = "" + apples.getFruitCount() + "/"+apples.getMaxFruits();
+                label6.Text = "" + oranges.getFruitCount() + "/"+oranges.getMaxFruits();
+                label7.Text = "" + grapes.getFruitCount() + "/"+grapes.getMaxFruits();
+                label8.Text = "" + watermelons.getFruitCount() + "/"+watermelons.getMaxFruits();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
